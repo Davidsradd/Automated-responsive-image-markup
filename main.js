ea96edc06fd5ec6acec707e3558fb730cfbec65d
Davidsradd <57205283+Davidsradd@users.noreply.github.com>
2020-04-03 19:57:02 -0500

    updated assignment

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-alert("hello");
+const IMAGES = document.querySelectorAll("img");
+const SIZES = {
+    showcase: "100vw"
+};
+
+function makeSrcset(imgSrc){
+    let markup = [];
+    let width = 400;
+
+    for (let i = 0; i<2; i++){
+        markup[i]= imgSrc + "-" + width + ".png" + width + "w";
+        width+=400;
+    }
+    return markup.join();
+}
+
+for (let i = 0; i <IMAGES.length; i ++) {
+    let imgSrc = IMAGES[i].getAttribute("src");
+    imgSrc = imgSrc.slice(0,-8);
+    let srcset = makeSrcset(imgSrc);
+    IMAGES[i].setAttribute("srcset", srcset);
+    console.log(srcset);
+
+    let type = IMAGES[i].getAttribute("data-type");
+    let sizes =SIZES[type];
+    IMAGES[i].setAttribute("sizes", sizes);
+    console.log(sizes);
+}
